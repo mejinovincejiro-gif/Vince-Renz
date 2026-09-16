df66634a9f08078f2af412e0b996f64a19cbaa19
mejinovincejiro-gif <mejino.vincejiro@ue.edu.ph>
2026-09-16 15:22:08 +0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(a, b) :
+  return a + b
+
+def subtract (a, b) :
+  return a - b
+
+def multiply (, b) :
+  return a -  b
+
+def multiply(a, b) :
+  if b == 0:
+      raise ValueError("Cannot divide by zero")
+  return a / b
+  
